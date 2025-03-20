b42dc9729cb315497aa5c90cb3e7f6d0d202d4bc
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-20 00:02:01 -0300

    fix: rust tokiopg

--- rust_tokiopg/src/main.rs
+++ rust_tokiopg/src/main.rs
@@ -5,37 +5,13 @@ use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let start_time = Instant::now();
+
     let connection_url = env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL not set");
 
     let (client, connection) = tokio_postgres::connect(&connection_url, NoTls).await?;
 
     tokio::spawn(connection);
-
-    let start_time = Instant::now();
-
-    client
-        .execute(
-            "
-            CREATE TABLE IF NOT EXISTS test_table (
-                id SERIAL PRIMARY KEY,
-                name TEXT NOT NULL
-            )
-            ",
-            &[],
-        )
-        .await?;
-
-    let rows: Vec<String> = (0..5_000_000)
-        .map(|i| format!("('{}')", format_args!("Item {}", i)))
-        .collect();
-
-    let insert_stmt: String = rows.join(", ");
-    client
-        .execute(
-            &format!("INSERT INTO test_table (name) VALUES {}", insert_stmt),
-            &[],
-        )
-        .await?;
 
     let rows = client.query("SELECT * FROM test_table", &[]).await?;
     for row in rows {
@@ -43,11 +19,6 @@ async fn main() -> Result<(), Error> {
         let _name: String = row.get(1);
         // println!("Row: {} - {}", id, name);
     }
-
-    client.execute("DELETE FROM test_table", &[]).await?;
-    client
-        .execute("DROP TABLE IF EXISTS test_table", &[])
-        .await?;
 
     let duration = start_time.elapsed();
     println!("Tempo total: {:.2?} segundos", duration.as_secs_f64());
